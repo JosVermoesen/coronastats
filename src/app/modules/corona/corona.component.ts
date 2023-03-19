c4598940bf8faad1238d491ebf9561192e80e02c
Jos Vermoesen <JosVermoesen@users.noreply.github.com>
2023-03-19 09:25:57 +0100

    using target

--- src/app/modules/corona/corona.component.ts
+++ src/app/modules/corona/corona.component.ts
@@ -113,9 +113,8 @@ export class CoronaComponent {
 
   getAllCountries() {
     this.apiWaiting = true;
-    const countries = this.cs.getAllCountries();
-    countries.subscribe(
-      (data: ICountry[]) => {
+    this.cs.getAllCountries().subscribe({
+      next: (data: ICountry[]) => {
         this.coronaCountries = data;
         this.preferdCountry = localStorage.getItem(
           'coronaApi_DefaultCountryName'
@@ -126,65 +125,69 @@ export class CoronaComponent {
 
         if (this.preferdCountry) {
           this.selectedCountryName = this.preferdCountry;
-          this.getCountryDetails(tmpCountryIndex ? +tmpCountryIndex : 0);
+          this.setCountryDetails(tmpCountryIndex ? +tmpCountryIndex : 0);
           this.getHistory(this.preferdCountry);
         } else {
-          this.getCountryDetails(0);
+          this.setCountryDetails(0);
         }
-
         this.apiWaiting = false;
         // console.log(this.coronaCountries);
       },
-      (err) => {
+      error: (err) => {
         console.log(err);
         this.apiWaiting = false;
         this.apiError = true;
-      }
-    );
+      },
+    });
   }
 
   getGlobalNumbers() {
     this.apiWaiting = true;
-    const corona = this.cs.getGlobal();
-    corona.subscribe(
-      (data: IWorld) => {
+    this.cs.getGlobal().subscribe({
+      next: (data: IWorld) => {
         this.coronaGlobalNumbers = data;
         // console.log(this.coronaGlobalNumbers);
         this.globalLastUpdate = new Date(this.coronaGlobalNumbers.updated);
         this.getAllCountries();
         this.apiWaiting = false;
       },
-      (err) => {
+      error: (err) => {
         console.log(err);
         this.apiWaiting = false;
         this.apiError = true;
-      }
-    );
+      },
+    });
   }
 
-  getCountryDetails(indexInList: number) {
-    this.countryHistorical;
+  setCountryDetailsWithTarget(target: any) {
+    const indexInList: number = target.selectedIndex;
+    if (indexInList) {
+      this.setCountryDetails(indexInList);
+    }
+  }
+
+  setCountryDetails(index: number) {
+    // this.countryHistorical;
     if (this.coronaCountries) {
-      this.coronaCountry = this.coronaCountries[indexInList];
+      this.coronaCountry = this.coronaCountries[index];
       this.selectedCountryName = this.coronaCountry.country;
-      this.selectedCountryIndex = indexInList;
+      this.selectedCountryIndex = index;
       this.countryLastUpdate = new Date(this.coronaCountry.updated);
     }
   }
 
   getHistory(country: string) {
-    const historical = this.cs.getCountryHistorical(country, this.evalDays);
-    historical.subscribe(
-      (data: ICountryHistorical) => {
+    this.cs.getCountryHistorical(country, this.evalDays).subscribe({
+      next: (data: ICountryHistorical) => {
         this.countryHistorical = data;
         this.prepareGraph();
       },
-      (err) => {
+      error: (err) => {
         console.log(err);
         this.coronaGlobalNumbers = null;
         this.apiError = true;
-      }
-    );
+      },
+    });
   }
 
   prepareGraph() {
